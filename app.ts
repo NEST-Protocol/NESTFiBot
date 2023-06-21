9f933c43179f286e5e91a17ea030b6fc9b2deb05
tunogya <tunogya@qq.com>
2023-06-21 23:31:06 +0800

    update

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@
 import http from "serverless-http";
 import {Telegraf} from "telegraf";
 import {Configuration, OpenAIApi, ChatCompletionRequestMessage} from "openai";
+import fetch from "node-fetch";
 
 const token = process.env.BOT_TOKEN!;
 const bot = new Telegraf(token);
@@ -11,14 +12,29 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-const binance_host = "https://api.binance.com"
-// /api/v3/ticker/price
-
 const getTokenPrice = async (token: string) => {
-  const price = await fetch(`${binance_host}/api/v3/ticker/price?symbol=${token}USDT`);
+  const price = await fetch(`https://api.nestfi.net/api/oracle/price/${token.toLowerCase()}usdt`);
   const json = await price.json();
-  return json.price;
+  // @ts-ignore
+  return `${json?.value}`;
 }
+
+const functions = [
+  {
+    name: "get_token_price",
+    description: "Get latest token price from binance",
+    parameters: {
+      type: "object",
+      properties: {
+        token: {
+          type: "string",
+          description: "token symbol, e.g. NEST, BTC, ETH, DOGE, XRP, MATIC, ADA, BNB, etc.",
+        }
+      },
+      required: ["token"],
+    }
+  }
+]
 
 bot.on("text", async (ctx) => {
   try {
@@ -32,59 +48,38 @@ bot.on("text", async (ctx) => {
     const chatCompletion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo-0613",
       messages: messages,
-      functions: [
-        {
-          name: "get_token_price",
-          description: "Get latest token price from binance",
-          parameters: {
-            type: "object",
-            properties: {
-              token: {
-                type: "string",
-                description: "token symbol, e.g. NEST, BTC, ETH, DOGE, XRP, MATIC, ADA, BNB, etc.",
-              }
-            },
-            required: ["token"],
-          }
-        }
-      ],
+      functions: functions,
       function_call: "auto",
       max_tokens: 250,
     });
     const response_message = chatCompletion.data.choices[0].message
     // check if the response is a function call
     if (response_message?.function_call) {
-      const available_functions = {
-        "get_token_price": getTokenPrice
-      }
       const function_name = response_message.function_call.name;
-      if (!function_name) throw new Error(`Function ${function_name} not found`);
       // @ts-ignore
-      const function_to_call = available_functions[function_name];
-      if (!function_to_call) throw new Error(`Function ${function_name} not found`);
-      const function_args = response_message.function_call.arguments;
-      if (!function_args) throw new Error(`Function ${function_name} arguments not found`);
-      // @ts-ignore
-      const function_response = await function_to_call(function_args?.token);
-      // append the function response to the messages
-      messages.push({
-        role: "function",
-        name: function_name,
-        content: function_response
-      })
-      const second_response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo-0613",
-        messages: messages,
-        max_tokens: 250,
-      });
-      // @ts-ignore
-      ctx.reply(second_response.data.choices[0].message.content);
-      return
+      const function_args = JSON.parse(response_message.function_call.arguments);
+      if (function_name === "get_token_price") {
+        const function_response = await getTokenPrice(function_args?.token);
+        // append the function response to the messages
+        messages.push({
+          role: "function",
+          name: function_name,
+          content: function_response
+        })
+        const second_response = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo-0613",
+          messages: messages,
+          max_tokens: 250,
+        });
+        // @ts-ignore
+        ctx.reply(second_response.data.choices[0].message.content);
+        return
+      }
     }
     // @ts-ignore
     ctx.reply(chatCompletion.data.choices[0].message.content);
   } catch (e) {
-    ctx.reply("Sorry, I don't understand that.");
+    ctx.reply(String(e));
   }
 })
 
