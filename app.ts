90238ac4237a93cda946aa73110085ea3f40fc8f
tunogya <tunogya@qq.com>
2023-06-21 20:44:05 +0800

    update

--- app.ts
+++ app.ts
@@ -8,20 +8,24 @@ const bot = new Telegraf(token);
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-  organization: process.env.OPENAI_ORGANIZATION,
 });
 const openai = new OpenAIApi(configuration);
 
 bot.on("text",  async (ctx) => {
-  const chatCompletion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {role: "system", content: "You are NEST Protocol Community Manager. You are talking to a user who is asking for help with a problem."},
-      {role: "user", content: ctx.message.text}
-    ],
-  });
-  // @ts-ignore
-  ctx.reply(chatCompletion.data.choices[0].message || "No response");
+  try {
+    const chatCompletion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {role: "system", content: "You are NEST Protocol Community Manager. You are talking to a user who is asking for help with a problem."},
+        {role: "user", content: ctx.message.text}
+      ],
+    });
+    // @ts-ignore
+    ctx.reply(chatCompletion.data.choices[0].message.content);
+  } catch (e) {
+    console.log(e);
+    ctx.reply(String(e));
+  }
 })
 
 export const handler = http(bot.webhookCallback("/bot"));
