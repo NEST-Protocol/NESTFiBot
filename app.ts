e39eac00de9efd644d90c3e7b9bbfa62d97b28e5
tunogya <tunogya@qq.com>
2023-08-11 16:09:37 +0800

    update

--- app.ts
+++ app.ts
@@ -6,6 +6,12 @@ import {message} from "telegraf/filters";
 const token = process.env.BOT_TOKEN!;
 const bot = new Telegraf(token);
 
+/**
+  start - Welcome to NESTFi Bot
+  stop - Delete authorization of NESTFi
+  help - How to use NESTFi Bot for telegram
+ **/
+
 // What can this bot do?
 bot.start(async (ctx) => {
   const user = ctx.from;
@@ -21,16 +27,17 @@ bot.start(async (ctx) => {
       ctx.reply(`Welcome back, ${user.username}`)
     } else {
       const code = Math.random().toString(36).substring(2, 18);
-        await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/set/code:${code}?EX=600`, {
-          method: 'POST',
-          headers: {
-            "Authorization": `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`
-          },
-          body: JSON.stringify(user)
-        })
-        ctx.reply('Hi here! Please authorize me tp set up a NESTFi integration.', Markup.inlineKeyboard([
-          Markup.button.url('Authorize me', `https://nest-fi-bot-web.vercel.app/${code}`)
-        ]))
+      await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/set/code:${code}?EX=600`, {
+        method: 'POST',
+        headers: {
+          "Authorization": `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`
+        },
+        body: JSON.stringify(user)
+      })
+      ctx.reply('Hi here! Please authorize me tp set up a NESTFi integration.', Markup.inlineKeyboard([
+        [Markup.button.url('Authorize me', `https://nest-fi-bot-web.vercel.app/${code}`)],
+        [Markup.button.url('Authorize in Metamask', `https://metamask.app.link/dapp/nest-fi-bot-web.vercel.app/${code}`)],
+      ]))
     }
   } catch (e) {
     ctx.reply(`Something went wrong: ${e}`)
@@ -38,11 +45,34 @@ bot.start(async (ctx) => {
   }
 });
 
-bot.command('menu', async () => {
+bot.command('help', (ctx) => {
+  ctx.replyWithMarkdownV2(`I can help you handle NESTFi futures. If you're new to NESTFi, please [see the website](https://nestfi.org).
 
+You can control me by sending these commands:
+
+/start - Welcome to NESTFi Bot
+/stop - Delete authorization of NESTFi
+/help - How to use NESTFi Bot for telegram
+`)
 })
 
-bot.command('account', async (ctx) => {
+// Stop command use to  delete authorization request
+bot.command('stop', async (ctx) => {
+  ctx.reply('You are about to delete your NESTFi authorization in this bot. Is that correct?', Markup.inlineKeyboard([
+    [Markup.button.callback('Yes, delete it', 'logout')],
+    [Markup.button.callback('No', 'menu')],
+  ]))
+})
+
+// Handle logout button click
+bot.action('logout', async (ctx) => {
+  const user = ctx.update.callback_query.from;
+  await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/del/auth:${user.id}`, {
+    headers: {
+      "Authorization": `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`
+    }
+  })
+  ctx.reply('You have successfully deleted your NESTFi authorization.')
 })
 
 bot.help((ctx) => ctx.reply('Send me a sticker'));
