1a194a16b730e53a4a518d7c4af661342143fdb4
tunogya <tunogya@qq.com>
2023-08-11 12:22:50 +0800

    update

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import http from "serverless-http";
 import {Markup, Telegraf} from "telegraf";
-import { Redis } from '@upstash/redis';
+import {Redis} from '@upstash/redis';
 import fetch from "node-fetch";
 import {message} from "telegraf/filters";
 
@@ -14,30 +14,40 @@ const redis = new Redis({
   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 })
 
-bot.start( async (ctx) => {
-  const user = ctx.message.from;
-  const authorization = await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/get/auth:${user.id}`, {
-    headers: {
-      "Authorization": `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`
-    }
-  })
-    .then(response => response.json())
-    .then((data: any) => data.result)
-
-  if (authorization) {
-    ctx.reply(`Welcome back, ${user.username}`)
-  } else {
-    const code = Math.random().toString(36).substring(2, 18);
-    await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/set/code:${code}?EX=600`, {
+bot.start(async (ctx) => {
+  const user = ctx.from;
+  try {
+    const authorization = await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/get/auth:${user.id}`, {
       headers: {
         "Authorization": `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`
-      },
-      body: JSON.stringify(user)
+      }
     })
-    ctx.reply('Hi here! Please authorize me tp set up a NESTFi integration.', Markup.inlineKeyboard([
-      Markup.button.url('Authorize me', `https://nest-fi-bot-web.vercel.app/?code=${code}`)
-    ]))
+      .then(response => response.json())
+      .then((data: any) => data.result)
+    if (authorization) {
+      ctx.reply(`Welcome back, ${user.username}`)
+    } else {
+      ctx.reply('Authorize me')
+    }
+  } catch (e) {
+    ctx.reply('Something went wrong')
+    console.log(e)
   }
+
+  // if (authorization) {
+  //   ctx.reply(`Welcome back, ${user.username}`)
+  // } else {
+  //   const code = Math.random().toString(36).substring(2, 18);
+  //   await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/set/code:${code}?EX=600`, {
+  //     headers: {
+  //       "Authorization": `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`
+  //     },
+  //     body: JSON.stringify(user)
+  //   })
+  //   ctx.reply('Hi here! Please authorize me tp set up a NESTFi integration.', Markup.inlineKeyboard([
+  //     Markup.button.url('Authorize me', `https://nest-fi-bot-web.vercel.app/?code=${code}`)
+  //   ]))
+  // }
 });
 
 bot.command('menu', async () => {
