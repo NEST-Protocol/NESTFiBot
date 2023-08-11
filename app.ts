c46ca330cd4ac6f29ba54d476bc7145db43b3351
tunogya <tunogya@qq.com>
2023-08-11 16:29:48 +0800

    update

--- app.ts
+++ app.ts
@@ -1,15 +1,14 @@
 import http from "serverless-http";
 import {Markup, Telegraf} from "telegraf";
 import fetch from "node-fetch";
-import {message} from "telegraf/filters";
 
 const token = process.env.BOT_TOKEN!;
 const bot = new Telegraf(token);
 
 /**
-  start - Welcome to NESTFi Bot
-  stop - Delete authorization of NESTFi
-  help - How to use NESTFi Bot for telegram
+ start - Welcome to NESTFi Bot
+ stop - Delete authorization of NESTFi
+ help - How to use NESTFi Bot for telegram
  **/
 
 // What can this bot do?
@@ -34,10 +33,15 @@ bot.start(async (ctx) => {
         },
         body: JSON.stringify(user)
       })
-      ctx.reply('Hi here! Please authorize me tp set up a NESTFi integration.', Markup.inlineKeyboard([
-        [Markup.button.url('Authorize me', `https://nest-fi-bot-web.vercel.app/${code}`)],
-        [Markup.button.url('Authorize in Metamask', `https://metamask.app.link/dapp/nest-fi-bot-web.vercel.app/${code}`)],
-      ]))
+      ctx.reply(`Hi here! Please authorize me tp set up a NESTFi integration. 
+
+*Note*: the authorization only works in 30 days.`, {
+        parse_mode: 'Markdown',
+        ...Markup.inlineKeyboard([
+          [Markup.button.url('Authorize me', `https://nest-fi-bot-web.vercel.app/${code}`)],
+          [Markup.button.url('Authorize in Metamask', `https://metamask.app.link/dapp/nest-fi-bot-web.vercel.app/${code}`)],
+        ])
+      })
     }
   } catch (e) {
     ctx.reply(`Something went wrong: ${e}`)
@@ -45,16 +49,18 @@ bot.start(async (ctx) => {
   }
 });
 
-bot.command('help', (ctx) => {
-  ctx.replyWithMarkdownV2(`I can help you handle NESTFi futures. If you're new to NESTFi, please [see the website](https://nestfi.org).
+bot.help((ctx) => {
+  ctx.reply(`I can help you handle NESTFi futures. If you're new to NESTFi, please [see the website](https://nestfi.org).
 
 You can control me by sending these commands:
 
 /start - Welcome to NESTFi Bot
 /stop - Delete authorization of NESTFi
 /help - How to use NESTFi Bot for telegram
-`)
-})
+`, {
+    parse_mode: 'Markdown'
+  })
+});
 
 // Stop command use to  delete authorization request
 bot.command('stop', async (ctx) => {
@@ -72,11 +78,7 @@ bot.action('logout', async (ctx) => {
       "Authorization": `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`
     }
   })
-  ctx.reply('You have successfully deleted your NESTFi authorization.')
+  ctx.editMessageText('You have successfully deleted your NESTFi Bot.', Markup.inlineKeyboard([]))
 })
-
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
 export const handler = http(bot.webhookCallback("/bot"));
